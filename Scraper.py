aaccdf15a0ef4fb73f938d940fbd1d122613b410
gorskik <krzysiek.kg3@gmail.com>
2020-03-18 12:59:38 +0100

    data

--- Scraper.py
+++ Scraper.py
@@ -1,22 +1,52 @@
-#import bibliotek
+# import libs
 import requests
 from bs4 import BeautifulSoup
+# adres strony z opiniami 
+url = "https://www.ceneo.pl/76367847#tab=reviews"
+# pobierz kod strony html
+page_response = requests.get(url)
+page_tree = BeautifulSoup(page_response.text, "html.parser")
+# pobranie z kodu strony fragmetów poszczególnych opinii 
+opinions = page_tree.select('li.review-box')
 
-#adres URL strony z opiniami
-url="https://www.ceneo.pl/76891706#tab=reviews"
+#składowe opinii
+for opinion in opinions:
+    opinion = opinions[0]
+    opinion_id = opinion["data-entry-id"]
+    author = opinion.select('div.reviewer-name-line').pop().string.strip()
+    stars = opinion.select('span.review-score-count').pop().string
+    purchased = opinion.select('div.product-review-pz').pop().string
+    useful = opinion.select('button.vote-yes').pop()["data-total-vote"]
+    useless = opinion.select('button.vote-no').pop()["data-total-vote"]
+    content = opinion.select("p.product-review-body").pop().get_text()
+    date = opinion.select("span.review-time > time")
+    review_date = date.pop(0)["datetime"]
 
-#pobranie kodu HTTML strony z adresu URL
-page  = requests.get(url)
-page_tree = BeautifulSoup(page.text, 'html.parser')
-print(page_tree.prettify())
-#wybranie kodu  strony z fragmentow
-opinions = page_tree.select("li.review-box")
+    try:
+        purchased = opinion.select('div.product-review-pz').pop().string
+    except IndexError:
+        purchased = None
+    try:
+        recommendation = opinion.select('div.product-review-summary > em').pop().string
+    except IndexError:
+        recommendation = None
+    try:
+        cons = opinion.select("div.cons-cell > ul").pop().get_text()
+    except IndexError:
+        cons = None
 
+    try:
+        pros = opinion.select("div.pros-cell > ul").pop().get_text()
+    except IndexError:
+        pros = None
 
-#ekstrakcj SKLADOWYCH dla 1 opnii z listy
+    try:
+        purchase_date = date.pop(0)["datetime"]
+    except IndexError:
+        purchase_date = None 
 
-opinion = opinions[1]
-id = opinion.select("li.review-box['data-entry-id']")
-author = opinion.select('div.reviewer-name-line')
-recomendation = opinion.select('div.product-review-summary > em')
-stars = opinion.select('span.review-score-count')
+    print(opinion_id, author, recommendation, stars, content, pros,
+      cons, useful, purchased, purchase_date, review_date)
+
+# - data wystawienia span .review-time > time ["datetime"] - pierwsze wyst
+# - data zakupu .review-time > time ["datetime"] - drugie wystąpienie
