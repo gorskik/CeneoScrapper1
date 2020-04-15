efd0bdf148371ef79d1f373bf032a7bdb53ea6fe
gorskik <krzysiek.kg3@gmail.com>
2020-04-15 13:24:34 +0200

    KG 15-04-2020

--- Scraper.py
+++ Scraper.py
@@ -4,6 +4,7 @@ import pprint
 import json
 from bs4 import BeautifulSoup
 
+
 #funkcja ekstrakcja składowych opinii
 def extract_feature(opinion, selector, attribute = None):
     try:
@@ -16,25 +17,25 @@ def extract_feature(opinion, selector, attribute = None):
 #lista składowych opinii wraz z selektorami i atrybutami
 selectors = {
             "author":['div.reviewer-name-line'],
-            "recomendation":['div.product-review-summary > em'],
-            "stars":['span.review-score.count'],
+            "recommendation":['div.product-review-summary > em'],
+            "stars":['span.review-score-count'],
             "content":['p.product-review-body'],
             "pros":['div.pros-cell > ul'],
             "cons":['div.cons-cell > ul'],
             "useful":['button.vote-yes',"data-total-vote"],
             "useless":['button.vote-no',"data-total-vote"],
             "purchased":['div.product-review-pz'],
-            "purchase_date":['span.review-time > time:nth-of-type(2)',"datetime"],
-            "review_date":['span.review-time > time:nth-of-type(1)',"datetime"]
+            "purchase_date":['span.review-time > time:nth-of-type(1)',"datetime"],
+            "review_date":['span.review-time > time:nth-of-type(2)',"datetime"]
         }
-#funkcja do usuwania znakow formatujacych
+
 def remove_whitespaces(text):
-        try:
-            for char in ["\n", "\r"]:
-                text.replace(char, ". ")
-            return text
-        except AttributeError:
-            pass
+    try:
+        for char in ["\n", "\r"]:
+            text = text.replace(char, ". ")
+        return text
+    except AttributeError:
+        pass
 #adres URL strony z opiniami
 url_prefix = "https://www.ceneo.pl"
 product_id = input("Podaj kod produktu: ")
@@ -53,17 +54,18 @@ while url is not None:
                     for key, args in selectors.items()}
         features["opinion_id"] = int(opinion["data-entry-id"])
         features["purchased"] = True if features["purchased"] == "Opinia potwierdzona zakupem" else False
-        features["usefull"]= int(features["useful"])
-        features["useless"]= int(features["useless"])
-        features["content"]= features["content"].replace("\n", ". ").replace("\r",". ")
+        features["useful"] = int(features["useful"])
+        features["useless"] = int(features["useless"])
+        features["content"] = remove_whitespaces(features["content"])
         try:
-            features["pros"]= features["pros"].replace("\n", ". ")
-        except AttributeError: 
+            features["pros"] = remove_whitespaces(features["pros"])
+        except:
             pass
         try:
-            features["cons"]= features["cons"].replace("\n", ". ")
-        except AttributeError:
+            features["cons"] = remove_whitespaces(features["cons"])
+        except:
             pass
+        
         opinions_list.append(features)
 
     try:
@@ -72,8 +74,8 @@ while url is not None:
         url = None
     print(url)
 # filename = product_id+".json"
-with open("opinions/"+product_id+".json",'w',encoding="utf-8") as fp:
-    json.dump(opinions_list,fp,ensure_ascii=False, separators=(",",": "),indent=4)
+with open("opinions/"+product_id+".json", 'w', encoding="UTF-8") as fp:
+    json.dump(opinions_list, fp, ensure_ascii=False, separators=(",",": "), indent=4 )
 # print(opinions_list)
 # for opinion in opinions_list:
 #     # pprint.pformat(opinion)
