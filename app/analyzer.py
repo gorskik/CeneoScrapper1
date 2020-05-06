e581a4649b08c372142e434833e100dec8a8428a
gorskik <krzysiek.kg3@gmail.com>
2020-05-06 14:50:45 +0200

    KG 06-05

--- app/analyzer.py
+++ app/analyzer.py
@@ -10,7 +10,7 @@ print(*os.listdir("./opinions_json"))
 #wczytanie identyfikatora produktu, którego opinie będą analizowane
 product_id = input("Podaj kod produktu: ")
 
-#wczytanie do ramki danych opinii z pliku
+#wczytanie do ramki danych opinii z plikudfdfd
 opinions = pd.read_json("./opinions_json/"+product_id+'.json')  
 opinions = opinions.set_index("opinion_id") 
 
--- app/forms.py
+++ app/forms.py
@@ -4,11 +4,12 @@ from wtforms.validators import DataRequired, Length, Regexp
 
 class ProductForm(FlaskForm):
     product_id = StringField(
-        "Podaj kod produktu z serwisu Ceneo.pl: ",
+        'Podaj kod produktu z serwisu Ceneo.pl: ',
         validators=[
-            DataRequired("Musisz podać kod produktu!"),
-            Length(min=8, max=8, message="Kod produktu musi mieć 8 znaków!"),
-            Regexp("^[0-9]{8}$", message="Kod produktu może zawierać tyko cyfry!")
+            DataRequired('Musisz podać kod produktu!'),
+            Length(min=8, max=8, message='Kod produktu musi mieć osiem znaków!'),
+            Regexp('^[0-9]+$', message='Kod produktu może zawierać tylko cyfry!')
         ]
     )
-    submit = SubmitField("Pobierz")
+    submit = SubmitField('Pobierz')
+© 2020 GitHub, Inc.
